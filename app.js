920eeeaefb7654e74d255352dc8a592f9b181a82
NIMO HERSI <UMULUQMAN12@GMAIL.COM>
2023-09-23 02:07:43 -0700

    demo ACP

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log("Iam in the HTML");
+alert("welcome to my page");
+
+const userName = prompt("what is your name")
+console.log(userName);
+
+
+
+
+document.write("Hi Welcome to my Self Love page!");
+ 
+
+if (userName == "ben"){
+    alert("Ken!");
+ } else if (userName == "Kassie"){
+    alert("Kassie is the best!!");
+ } 
+
+
+         
