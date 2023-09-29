245408470f0693fa07be58f8b91e192535a38881
NIMO HERSI <UMULUQMAN12@GMAIL.COM>
2023-09-28 17:11:02 -0700

    demo ACP

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 'use strict';
 
-console.log("Iam in the HTML");
-alert("welcome to my page");
+console.log("I am in the HTML");
+alert("Welcome to my page");
+const theirName = getName();
+myAudience(theirName);
 
-const userName = prompt("what is your name")
-console.log(userName);
+function getName() {
+  const userName = prompt("What is your name?");
+  return userName;
+}
 
+function myAudience(userName) {
+  document.write("Hi " + userName + ", welcome to my Self Love page!");
+  return;
+}
 
-
-document.write("Hi Welcome to my Self Love page!");
-
-
- 
- if((10 == 10) && ( 4==2)){
-   console.log("true");
- } else {
-   console.log("false");
- }
-
-
-
- function greeting (time){
-  if (time < 20){
-   return "Good Morning";
-  } else if (time <=30){
-   return  "Good Evening"; 
-  } else {
-   return "Good Night"
+function specialMessage(userName) {
+  if (userName === "") {
+    userName = prompt("Come on, please tell me your name!");
   }
- }  
+}
 
- console.log(greeting(24));
+if ((10 === 10) && (4 === 2)) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+function greeting(time) {
+  if (time < 20) {
+    return "Good Morning";
+  } else if (time <= 30) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
+}
+
+console.log(greeting(24));
