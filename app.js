c55f39d6c99e768dafe9b44b53862170d11c004a
NIMO HERSI <UMULUQMAN12@GMAIL.COM>
2023-09-30 00:12:00 -0700

    ACP

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 'use strict';
 console.log("I am in the HTML");
 alert("Welcome to my page");
+
+const theirName = getName();
+console.log (theirName);
+
+function getName(){
 const usersName = prompt("What is your name?");
-console.log(usersName);
-if (usersName === "Kassie") {
+return usersName;
+}
+
+function specialMessage(usersName){
+if (usersName === "Kassie"){
     alert("Hi Kassie");
 } else {
     alert("Good to have you here");
 }
+}
 
-document.write("Hi " + usersName + " welcome to my page!");
+function greetUser(){
+document.write("Hi " + usersName + " welcome to my self love my page!");
+} 
 
+function askAboutSelfLove(usersName){
 const question = prompt("What's one thing you love about yourself?");
+if (question==""){
+  alert("take your time to thing about it");
 
+}
+}
 
